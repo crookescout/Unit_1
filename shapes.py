5fac85dd3efd3947d325d0b21a21325aadb599b9
scout <scout.crooke@ssfs.org>
2019-09-11 08:54:20 -0400

    made some shapes

---
+++ shapes.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.left(75)
+turtle.forward(100)
+turtle.right(145)
+turtle.forward(100)
+turtle.right(145)
+turtle.forward(100)
+turtle.right(145)
+turtle.forward(100)
+turtle.right(145)
+turtle.forward(100)
+
+turtle.exitonclick()
